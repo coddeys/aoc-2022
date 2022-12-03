e0e0edd3fc4c010c5638d7177cfde210e6b30b16
Dima Sukhikh <dima@sukhikh.xyz>
2022-12-03 21:17:55 +0100

    Day 1 refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,13 +4,17 @@ import Data.List (sort)
 
 solveDay1 :: FilePath -> IO ()
 solveDay1 fp = do
-  inputLines <- readLines fp
-  print inputLines
+  calories <- (elvesMostCalories 3) <$> readCalories fp
+  print calories
+
+elvesMostCalories :: Int -> [Int] -> Int
+elvesMostCalories n =
+  sum . (lastN n) . sort
+
+readCalories :: FilePath -> IO [Int]
+readCalories fp = map sum <$> foldr elves [] <$> readLines fp
 
 type Elve = [Int]
-
-readLines :: FilePath -> IO (Int)
-readLines fp = sum <$> lastN 3 <$> sort <$> map sum <$> foldr elves [] <$> lines <$> readFile fp
 
 elves :: String -> [Elve] -> [Elve]
 elves s [] = [[read s]]
@@ -19,3 +23,6 @@ elves n (x : xs) = ((read n) : x) : xs
 
 lastN :: Int -> [a] -> [a]
 lastN n xs = drop (length xs - n) xs
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
